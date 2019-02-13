2770a874e844df656e885b4876d9a7c05f17cb64
Linus Weber <linus.weber@protonmail.com>
2019-02-13 07:33:16 +0100

    testing parser and sdl

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/linus4/csgoverview
 
-require github.com/veandco/go-sdl2 v0.3.0 // indirect
+require (
+	github.com/go-gl/gl v0.0.0-20181026044259-55b76b7df9d2 // indirect
+	github.com/go-gl/glfw v0.0.0-20181213070059-819e8ce5125f // indirect
+	github.com/gogo/protobuf v1.2.0 // indirect
+	github.com/golang/geo v0.0.0-20181008215305-476085157cff // indirect
+	github.com/llgcode/draw2d v0.0.0-20180825133448-f52c8a71aff0 // indirect
+	github.com/markus-wa/demoinfocs-golang v1.0.0 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/veandco/go-sdl2 v0.3.0 // indirect
+	golang.org/x/image v0.0.0-20190209060608-ef4a1470e0dc // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	winHeight int32 = 926
+	winWidth  int32 = 926
+)
+
 func main() {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		log.Println(err)
+	}
+	defer sdl.Quit()
+
 	window, err := sdl.CreateWindow("csgoverview", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
+		winHeight, winWidth, sdl.WINDOW_SHOWN)
 
 	if err != nil {
-		fmt.Println(err)
-		sdl.Quit()
+		log.Println(err)
 		return
 	}
 	defer window.Destroy()
 
-	sdl.Delay(2000)
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	surface, err := img.Load("de_cache.jpg")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	texture, err := renderer.CreateTextureFromSurface(surface)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer texture.Destroy()
+
+	// nil, nil stretches texture to fill the screen
+	// err
+	renderer.Clear()
+	// err
+	renderer.Copy(texture, nil, nil)
+	renderer.Present()
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+		sdl.Delay(32)
+	}
+
+	/*
+		demo, err := os.Open("test.dem")
+		if err != nil {
+			log.Println("Could not open test.dem")
+			log.Println(err)
+			return
+		}
+		defer demo.Close()
+
+		parser := dem.NewParser(demo)
+		fmt.Println("Created parser")
+
+		states := make([]dem.IGameState, 0)
+		b := true
+		for b {
+			// err!
+			b, _ = parser.ParseNextFrame()
+			states = append(states, parser.GameState())
+		}
+		for i, gs := range states {
+			fmt.Printf("Frame %v: Grenades: %v\n", i, gs.TotalRoundsPlayed())
+		}
+		fmt.Println("Finished parsing the demo")
+	*/
+
+	// get information from header (map, tickrate)
+
+	// find matchstart, round strats, half-time, overtime half starts
+
+	// parse and present demo frame by frame (positions)
+
+	// translate coordinates
+
+	// draw entities
 }
