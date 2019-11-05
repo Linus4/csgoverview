73ca15f7bc4333865d5c598a8902071e632a3b8e
Linus Weber <linus.weber@protonmail.com>
2019-11-05 04:55:58 +0100

    refactoring, build dependencies, windows paths %USERDIR\csgoverview, linux finding font fixed

---
+++ main_windows.go
@@ -0,0 +1,34 @@
+// +build windows
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	fontName string = "DejaVuSans"
+)
+
+func main() {
+	conf := DefaultConfig
+	flag.Float64Var(&conf.FrameRate, "framerate", conf.FrameRate, "Fallback GOTV Framerate")
+	flag.Float64Var(&conf.TickRate, "tickrate", conf.TickRate, "Fallback Gameserver Tickrate")
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln("trying to get user home directory:", err)
+	}
+	defaultFontPath := fmt.Sprintf("%v\\csgoverview\\%v.ttf", userHomeDir, fontName)
+	defaultOverviewDirectory := fmt.Sprintf("%v\\csgoverview\\", userHomeDir)
+	flag.StringVar(&conf.FontPath, "fontpath", defaultFontPath, "Path to font file (.ttf)")
+	flag.StringVar(&conf.OverviewDir, "overviewdir", defaultOverviewDirectory, "Path to overview directory")
+	flag.Parse()
+
+	err = run(&conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
