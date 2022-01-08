b13943855ba1fa8cd8d87286962d13d531488454
Michal Vala <michal.vala@gmail.com>
2022-01-08 23:04:11 +0100

    script to download overviews

---
+++ overviews.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p overviews
+curl -o overviews/de_overpass.jpg "https://i.imgur.com/VHWIjSB.jpeg"
+curl -o overviews/de_mirage.jpg "https://i.imgur.com/6HLCCp1.jpeg"
+curl -o overviews/de_vertigo.jpg "https://i.imgur.com/q8HmCHN.jpeg"
+curl -o overviews/de_vertigo_lower.jpg "https://i.imgur.com/qCbHmJ2.jpeg"
+curl -o overviews/de_nuke.jpg "https://i.imgur.com/frbUQtK.jpeg"
+curl -o overviews/de_nuke_lower.jpg "https://i.imgur.com/vaF05M5.jpeg"
+curl -o overviews/de_cache.jpg "https://i.imgur.com/KqtNKUc.jpeg"
+curl -o overviews/de_inferno.jpg "https://i.imgur.com/MW7Kp7f.jpeg"
+curl -o overviews/de_train.jpg "https://i.imgur.com/FSLMR6V.jpeg"
+curl -o overviews/de_dust2.jpg "https://i.imgur.com/OrsYdDq.jpeg"
+curl -o overviews/de_ancient.jpg "https://i.imgur.com/2ZWUOt3.jpeg"
