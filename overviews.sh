ac185f95864c0f21219bbf21e59affda6c86d24f
Michal Vala <michal.vala@gmail.com>
2022-01-09 14:42:11 +0100

    download map overviews from zoidbergwill/csgo-overviews githu repo

--- overviews.sh
+++ overviews.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
+MAPS="de_overpass de_mirage de_vertigo de_vertigo_lower de_nuke de_nuke_lower de_cache de_inferno de_train de_dust2 de_ancient"
+
 mkdir -p overviews
-curl -o overviews/de_overpass.jpg "https://i.imgur.com/VHWIjSB.jpeg"
-curl -o overviews/de_mirage.jpg "https://i.imgur.com/6HLCCp1.jpeg"
-curl -o overviews/de_vertigo.jpg "https://i.imgur.com/q8HmCHN.jpeg"
-curl -o overviews/de_vertigo_lower.jpg "https://i.imgur.com/qCbHmJ2.jpeg"
-curl -o overviews/de_nuke.jpg "https://i.imgur.com/frbUQtK.jpeg"
-curl -o overviews/de_nuke_lower.jpg "https://i.imgur.com/vaF05M5.jpeg"
-curl -o overviews/de_cache.jpg "https://i.imgur.com/KqtNKUc.jpeg"
-curl -o overviews/de_inferno.jpg "https://i.imgur.com/MW7Kp7f.jpeg"
-curl -o overviews/de_train.jpg "https://i.imgur.com/FSLMR6V.jpeg"
-curl -o overviews/de_dust2.jpg "https://i.imgur.com/OrsYdDq.jpeg"
-curl -o overviews/de_ancient.jpg "https://i.imgur.com/2ZWUOt3.jpeg"
+for MAP in ${MAPS}; do
+   echo "downloading ${MAP} ..."
+   curl -s -o "overviews/${MAP}.jpg" "https://raw.githubusercontent.com/zoidbergwill/csgo-overviews/master/overviews/${MAP}.jpg"
+done
