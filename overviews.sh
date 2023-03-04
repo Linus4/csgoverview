49b52e2162cd2686fb00b8dd5389405c8fdeb942
Linus Weber <linus.weber@protonmail.com>
2023-03-04 13:33:06 +0100

    added de_anubis to overview-download script

--- overviews.sh
+++ overviews.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAPS="de_overpass de_mirage de_vertigo de_vertigo_lower de_nuke de_nuke_lower de_cache de_inferno de_train de_dust2 de_ancient"
+MAPS="de_overpass de_mirage de_vertigo de_vertigo_lower de_nuke de_nuke_lower de_cache de_inferno de_train de_dust2 de_ancient de_anubis"
 
 mkdir -p overviews
 for MAP in ${MAPS}; do
